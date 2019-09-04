56fd80bd81e89fd487e9452b0ba6ba9f828cc034
mihai <12361504+deta15@users.noreply.github.com>
2019-09-04 11:38:36 +0300

    add yoast scehma.org for yoast breacrumbs

--- functions.php
+++ functions.php
@@ -39,3 +39,50 @@ function get_price_range_funtion_custom() {
 
 	return $method->invoke(new WC_Widget_Price_Filter);
 }
+
+//yoast breadcrumbs change schema.org/schema
+//special tanks to https://gist.github.com/beaverbuilder
+function bbtheme_yoast_breadcrumbs() {
+
+  if ( function_exists('yoast_breadcrumb') && ! is_front_page() ) {
+	
+	    yoast_breadcrumb('<div class="container"><p id="breadcrumbs"><ol itemscope itemtype="http://schema.org/BreadcrumbList">','</ol></p></div>');
+  }
+}
+
+add_action( 'the_core_action_after_header', 'bbtheme_yoast_breadcrumbs' );
+
+
+$breadcrumb_count = 1;
+
+add_filter( 'wpseo_breadcrumb_single_link', 'ss_breadcrumb_single_link', 10, 2 );
+
+function ss_breadcrumb_single_link( $link_output, $link ) {
+    
+    global $breadcrumb_count; 
+
+    $element = 'li';
+    $element = esc_attr( apply_filters( 'wpseo_breadcrumb_single_link_wrapper', $element ) );
+    $link_output = '<' . $element . ' itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">';
+    
+    if ( isset( $link['url'] )) {
+    	 
+        $link_output .= '<a itemprop="item" href="' . esc_url( $link['url'] ) . '"><span itemprop="name">' . esc_html( $link['text'] ) . '</span></a><meta itemprop="position" content="'.$breadcrumb_count++.'" />'; 
+    } 
+    
+    $link_output .= '</' . $element . '>';
+    return $link_output;
+}
+
+add_filter( 'wpseo_breadcrumb_output_wrapper', 'ss_breadcrumb_output_wrapper', 10, 1 );
+function ss_breadcrumb_output_wrapper( $wrapper) {
+     
+    $wrapper = 'ol';
+    return $wrapper;
+}
+
+// add_filter( 'wpseo_breadcrumb_output', 'ss_breadcrumb_output' );
+// function ss_breadcrumb_output($output) {
+//     var_dump($output); // you should see the string referred to above
+//     return apply_filters( 'ss_breadcrumb_output', '<' . $wrapper . $id . $class . ' xmlns:v="http://rdf.data-vocabulary.org/#">' . $output . '</' . $wrapper . '>' );
+// }
